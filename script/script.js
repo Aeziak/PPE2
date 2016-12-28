e5598549e6debad8b13f2bc5cedb4f1caf714187
Rockiste <lga.rockiste26@gmail.com>
2016-12-28 17:42:52 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -36,22 +36,14 @@ function getPosition(){
 
 }
 
-function Bidule(lat1,lng1,lat2,lng2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lng2-lng1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
-}
+function distance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+          c(lat1 * p) * c(lat2 * p) * 
+          (1 - c((lon2 - lon1) * p))/2;
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
 
 // echo test de la lat et lng	
@@ -76,8 +68,11 @@ while ( true ) {
 
 	var lat1 = parcours[i].lat;
 	var lat2 = parcours[i + 1].lat;
-	var lng1 = parcours[i].lng;
-	var lng2 = parcours[i + 1].lng;
+	var lon1 = parcours[i].lng;
+	var lon2 = parcours[i + 1].lng;
+
+	var d = distance(lat1, lon1, lat2, lon2);
+	console.log(d);
 
 }
 
